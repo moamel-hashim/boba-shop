6ec474678cc7bec6796ee558719623c92fd72091
moamel-hashim <muamal1997@gmail.com>
2023-11-14 16:43:36 -0800

    completed nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React from 'react'
 
 export default function Navbar() {
   return (
     <nav>
-      <div className="title">
-        <h2>Boba Shop</h2>
-      </div>
       <div className="tabs">
+        <div className='title'>
+          <h3>boba shop</h3>
+        </div>
         <input type="radio" name="home" />
         <input type="radio" name="menu" />
         <input type="radio" name="admin" />
+        <div className="buttons">
+          <label htmlFor="home">Home</label>
+          <label htmlFor="menu">Menu</label>
+          <label htmlFor="admin">Admin</label>
+          <div className="underline"></div>
+        </div>
       </div>
-      <div className="buttons">
-        <label htmlFor="home">Home</label>
-        <label htmlFor="menu">Menu</label>
-        <label htmlFor="admin">Admin</label>
-      </div>
-      <div className="underline"></div>
     </nav>
   )
 }
